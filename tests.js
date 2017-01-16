4a11672abeb085fe5386b65baba05c4e95d9568a
Pooja Saxena <pooja335@gmail.com>
2017-01-16 17:57:54 -0600

    find root algorithm working and tests pass

---
+++ tests.js
@@ -0,0 +1,46 @@
+var assert = require('assert');
+var findFrequency = require('./textFrequency');
+
+
+var inputString = 'fry fries fried frying spy spies spied sassed mulled willed willing gaffed gaffing fizzed talked talking booked booking ribbed ribbing checking padded padding hugged hugging yakked rammed running tipped stirred ratted revved checked potatoes heroes atlases masses sasses fizzes boxes fixes sketches marshes cats talks books atlas bus sass mass truss peas'
+findFrequency(inputString);
+
+var makeTest = function(text, counts) {
+  assert.deepEqual(findFrequency(text), counts);
+}
+
+// words that add an -es to pluralize
+makeTest('potato potatoes', { potato: 2 });
+makeTest('hero heroes', { hero: 2 });
+makeTest('mass masses', { mass: 2 });
+
+// double consonant at end in the case of -ed or -ing
+// keep 2nd letter
+makeTest('sass sasses sassed sassing', { sass: 4 });
+makeTest('mull mulls mulled mulling', { mull: 4 });
+makeTest('gaff gaffs gaffed gaffing', { gaff: 4 });
+makeTest('fizz fizzes fizzed fizzing', { fizz: 4 });
+// remove 2nd letter
+makeTest('pad pads padded padding', { pad: 4 });
+makeTest('rib ribs ribbed ribbing', { rib: 4 });
+makeTest('yak yaks yakked yakking', { yak: 4 });
+makeTest('ram rams rammed ramming', { ram: 4 });
+makeTest('stir stirs stirred stirring', { stir: 4 });
+
+// // words that add only -d (vs -ed) for past tense
+// makeTest('love loves loved loving', { love: 4 });
+// makeTest('rate rates rated rating', { rate: 4 });
+
+// words ending in [consonant]-y
+makeTest('fry fries fried frying', { fry: 4 });
+makeTest('spy spies spied spying', { spy: 4 });
+
+// normal
+makeTest('talk talks talked talking', { talk: 4 });
+makeTest('book books booked booking', { book: 4 });
+
+
+makeTest('box boxes boxed boxing', { box: 4 });
+makeTest('sketch sketches sketched sketching', { sketch: 4 });
+
+console.log('All tests passed!')
---
+++ textFrequency.js
@@ -0,0 +1,89 @@
+var rules = [
+  { // if word ends with -ies or -ied, remove those 3 letters and add -y
+    matcher: /\w+ie[sd]$/,
+    stemmer: function(str) {
+      return str.slice(0, -3) + 'y';
+    }
+  },
+  { // if word ends with -[o, s, z, x, ch, sh]es, remove -es
+    matcher: /\w+(?:[oszx]|ch|sh)es$/,
+    stemmer: function(str) {
+      return str.slice(0, -2);
+    }
+  },
+  { // if word ends with -s and previous letter is not s, remove -s
+    // known issue: this will cause problems for singular words ending with -s
+    matcher: /\w+[^s]s$/,
+    stemmer: function(str) {
+      return str.slice(0, -1);
+    }
+  },
+  { // if word ends with -ed after double consonants f, l, s, z, remove -ed
+    matcher: /\w+([flsz])\1ed$/,
+    stemmer: function(str) {
+      return str.slice(0, -2);
+    }
+  },
+  { // if word ends with -ing after double consonants f, l, s, z, remove -ing
+    matcher: /\w+([flsz])\1ing$/,
+    stemmer: function(str) {
+      return str.slice(0, -3);
+    }
+  },
+  { // if word ends with -ed after double consonants other than f, l, s, z, 
+      // remove -[consonant]ed
+    matcher: /\w+([^flsz])\1ed$/,
+    stemmer: function(str) {
+      return str.slice(0, -3);
+    }
+  },
+  { // if word ends with -ing after double consonants other than f, l, s, z, 
+      // remove -[consonant]ing
+    matcher: /\w+([^flsz])\1ing$/,
+    stemmer: function(str) {
+      return str.slice(0, -4);
+    }
+  },
+  { // if word ends with -ed, remove -ed
+    // known issue: this will cause problems for words ending in e (skated, etc)
+    matcher: /\w+ed$/,
+    stemmer: function(str) {
+      return str.slice(0, -2);
+    }
+  },
+  { // if word ends with -ing, remove -ing
+    // known issue: this will cause problems for words ending in e (skating, etc)
+    matcher: /\w+ing$/,
+    stemmer: function(str) {
+      return str.slice(0, -3);
+    }
+  },
+]
+
+var findRoot = function(word) {
+  for (var i = 0; i < rules.length; i++) {
+    if (rules[i].matcher.test(word)) {
+      return rules[i].stemmer(word);
+    }
+  }
+  return word;
+};
+
+var findFrequency = function (text) {
+  text = text.toLowerCase().split('');
+  var textNoPunc = text.filter(function (char) {
+    return (/[\w\s]/).test(char);
+  }).join('');
+  var textArray = textNoPunc.split(' ');
+  for (var i = 0; i < textArray.length; i++) {
+    textArray[i] = findRoot(textArray[i])
+  }
+  console.log(textArray);
+  var count = {};
+  for (var ii = 0; ii < textArray.length; ii++) {
+    count[textArray[ii]] = (count[textArray[ii]] || 0) + 1;
+  }
+  return count
+};
+
+module.exports = findFrequency;
