1bc73230d1291f9a3d1743df1fea5fc5e8355ed0
Pooja Saxena <pooja335@gmail.com>
2017-01-19 10:59:43 -0600

    checks for file of type text

--- index.js
+++ index.js
@@ -8,17 +8,20 @@ $(function () {
   })
 
   $('form').submit(function() {
-    console.log('here');
     var file = $('#fileInput')[0].files[0];
-    if (file) {
+    var fileExtension = file.name.substr(file.name.lastIndexOf('.') + 1)
+    if (fileExtension === 'txt' || fileExtension === file.name) {
       var reader = new FileReader();
-      reader.readAsText(file, "UTF-8");
+      reader.readAsText(file, 'UTF-8');
       reader.onload = function(evt) {
-        $('#results').append(evt.target.result);
+        $('#results').empty().append(evt.target.result);
+        //OR SHOULD I USE .html() or .text() instead??
       }
       reader.onerror = function (evt) {
-        $('#results').append("error reading file");
+        $('#results').empty().append('An error occurred. Please try again');
       }
+    } else {
+      $('#results').empty().append('Please make sure your file is a text file');
     }
     return false;
   })
